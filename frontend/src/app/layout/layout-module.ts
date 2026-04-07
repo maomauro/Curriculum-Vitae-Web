743f1d9efbf5dd324de06b9d5f6e576867e9b322
maomauro <maomauro.c@gmail.com>
2026-04-07 18:38:53 -0500

    refactor(frontend): flatten layout/components subfolders

--- frontend/src/app/layout/layout-module.ts
+++ frontend/src/app/layout/layout-module.ts
@@ -5,10 +5,10 @@ import { RouterModule } from '@angular/router';
 import { PublicLayoutComponent } from './containers/public-layout.component';
 import { AuthLayoutComponent } from './containers/auth-layout.component';
 import { AdminLayoutComponent } from './containers/admin-layout.component';
-import { NavbarPublicComponent } from './components/navbar-public/navbar-public.component';
-import { FooterPublicComponent } from './components/footer/footer.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { TopbarComponent } from './components/topbar/topbar.component';
+import { NavbarPublicComponent } from './components/navbar-public.component';
+import { FooterPublicComponent } from './components/footer.component';
+import { SidebarComponent } from './components/sidebar.component';
+import { TopbarComponent } from './components/topbar.component';
 
 @NgModule({
   declarations: [
